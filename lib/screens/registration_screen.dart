3498976185835d3647b6947ec83629f4a546bc0b
Wazhma Hakimi <133529903+WazhmaHakimi@users.noreply.github.com>
2026-06-20 22:54:31 +0430

    Added Modal Progress Hud sns package to project, and some visibility changes added.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,7 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_starting_project/screens/chat_screen.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../components/rounded_button.dart';
 import '/constants.dart';
@@ -19,98 +20,116 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   var auth = FirebaseAuth.instance;
   String errorMessage = '';
+  bool errorOccured = false;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(height: 48.0),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: kTextFieldDecoration.copyWith(
-                      hintText: 'Enter your password',
-                      labelText: 'Email',
+              const SizedBox(height: 48.0),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your password',
+                        labelText: 'Email',
+                      ),
+                      controller: _emailController,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (email) {
+                        return email != null && EmailValidator.validate(email)
+                            ? null
+                            : 'Please enter a valid email';
+                      },
                     ),
-                    controller: _emailController,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (email) {
-                      return email != null && EmailValidator.validate(email)
-                          ? null
-                          : 'Please enter a valid email';
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    decoration: kTextFieldDecoration.copyWith(
-                      hintText: 'Enter your password',
-                      labelText: 'Password',
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your password',
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                      controller: _passwordController,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (password) {
+                        return password != null && password.length > 5
+                            ? null
+                            : 'The password should be at least more than five characters.';
+                      },
                     ),
-                    obscureText: true,
-                    controller: _passwordController,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (password) {
-                      return password != null && password.length > 5
-                          ? null
-                          : 'The password should be at least more than five characters.';
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 24.0),
-            Text(
-              errorMessage,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.red, fontSize: 12),
-            ),
-            RoundedButton(
-              color: kRegisterButtonColor,
-              title: 'Register',
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  try {
-                    await auth
-                        .createUserWithEmailAndPassword(
-                          email: _emailController.text,
-                          password: _passwordController.text,
-                        )
-                        .then((value) {
-                          Navigator.pop(context);
-                          Navigator.pushNamed(context, ChatScreen.id);
-                        });
-                  } catch (e) {
-                    print('Error: ${e.toString}');
-                    setState(() {
-                      errorMessage = e.toString().split(']')[1];
-                    });
+              const SizedBox(height: 24.0),
+              Visibility(
+                visible: errorOccured,
+                child: Text(
+                  errorMessage,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
+              ),
+              RoundedButton(
+                color: kRegisterButtonColor,
+                title: 'Register',
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    try {
+                      setState(() {
+                        showSpinner = true;
+                        errorOccured = false;
+                      });
+
+                      await auth
+                          .createUserWithEmailAndPassword(
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                          )
+                          .then((value) {
+                            Navigator.pop(context);
+                            Navigator.pushNamed(context, ChatScreen.id);
+                          });
+
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (e) {
+                      setState(() {
+                        showSpinner = false;
+                        errorOccured = true;
+                        errorMessage = e.toString().split(']')[1];
+                      });
+                    }
                   }
-                }
-              },
-            ),
-            const SizedBox(height: 12),
-            IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+                },
+              ),
+              const SizedBox(height: 12),
+              IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
